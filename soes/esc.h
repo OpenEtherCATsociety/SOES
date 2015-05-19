c68888e40be20e9fe51428ecf35e65f575e2a83a
Hans-Erik Floryd <hans-erik.floryd@rt-labs.com>
2015-05-19 10:46:35 +0200

    Breakout configuration settings

--- soes/esc.h
+++ soes/esc.h
@@ -39,6 +39,7 @@
 #define __esc__
 
 #include <cc.h>
+#include <config.h>
 
 /* storage modifier for data stored in flashmemory */
 #define FLASHSTORE               const
@@ -166,35 +167,6 @@
 #define COE_VALUEINFO_DEFAULT           0x10
 #define COE_VALUEINFO_MINIMUM           0x20
 #define COE_VALUEINFO_MAXIMUM           0x40
-
-#define MBXSIZE                        0x80
-#define MBXSIZEBOOT                    0x80
-#define MBXBUFFERS                     3
-
-#define MBX0_sma                       0x1000
-#define MBX0_sml                       MBXSIZE
-#define MBX0_sme                       MBX0_sma+MBX0_sml-1
-#define MBX0_smc                       0x26
-#define MBX1_sma                       0x1080
-#define MBX1_sml                       MBXSIZE
-#define MBX1_sme                       MBX1_sma+MBX1_sml-1
-#define MBX1_smc                       0x22
-
-#define MBX0_sma_b                     0x1000
-#define MBX0_sml_b                     MBXSIZEBOOT
-#define MBX0_sme_b                     MBX0_sma_b+MBX0_sml_b-1
-#define MBX0_smc_b                     0x26
-#define MBX1_sma_b                     0x1080
-#define MBX1_sml_b                     MBXSIZEBOOT
-#define MBX1_sme_b                     MBX1_sma_b+MBX1_sml_b-1
-#define MBX1_smc_b                     0x22
-
-#define SM2_sma                        0x1100
-#define SM2_smc                        0x24
-#define SM2_act                        0x01
-#define SM3_sma                        0x1180
-#define SM3_smc                        0x20
-#define SM3_act                        0x01
 
 #define MBXHSIZE                       sizeof(_MBXh)
 #define MBXDSIZE                       MBXSIZE-MBXHSIZE
