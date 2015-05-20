96a620c2adf82a0414269dc75a543ddd38035de2
Hans-Erik Floryd <hans-erik.floryd@rt-labs.com>
2015-05-20 09:29:37 +0200

    cmake toolchain for kinetis

--- cmake/Platform/rt-kernel.cmake
+++ cmake/Platform/rt-kernel.cmake
@@ -6,8 +6,23 @@ set(_RT_KERNEL_CMAKE_ TRUE)
 
 cmake_minimum_required (VERSION 3.1.2)
 
+# Get environment variables
+set(RTK $ENV{RTK} CACHE STRING
+  "Location of rt-kernel tree")
+set(BSP $ENV{BSP} CACHE STRING
+  "The name of the BSP to build for")
+
 # Avoid warning when re-running cmake
 set(DUMMY ${CMAKE_TOOLCHAIN_FILE})
+
+include_directories(
+  ${RTK}/include/kern
+  ${RTK}/include/drivers
+  ${SOES_SOURCE_DIR}/include/sys/gcc
+  )
+link_directories(
+  ${RTK}/lib/${ARCH}/${CPU}
+  )
 
 # No support for shared libs
 set_property(GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS FALSE)
@@ -16,19 +31,15 @@ set(CMAKE_STATIC_LIBRARY_PREFIX "lib")
 set(CMAKE_STATIC_LIBRARY_SUFFIX ".a")
 set(CMAKE_EXECUTABLE_SUFFIX ".elf")
 
-#set(CMAKE_FIND_LIBRARY_PREFIXES "lib")
-#set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
+# Common compile flags
+add_compile_options(-Wall -Wextra -Wno-unused-parameter -Werror -fomit-frame-pointer -fno-strict-aliasing)
 
-# Get environment variables
-set(RTK $ENV{RTK} CACHE STRING
-  "Location of rt-kernel tree")
-set(COMPILERS $ENV{COMPILERS} CACHE STRING
-  "Location of compiler toolchain")
-set(BSP $ENV{BSP} CACHE STRING
-  "The name of the BSP to build for")
+# Add machine flags to C_FLAGS so they take effect for linking also
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${MACHINE_FLAGS}" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${MACHINE_FLAGS}" CACHE STRING "" FORCE)
 
-# Common flags
-add_definitions(-Wall -Wextra -Wno-unused-parameter -Werror -fomit-frame-pointer -fno-strict-aliasing -fshort-wchar)
+# Default libs
+set(RTK_LIBS "-l${BSP} -l${ARCH} -lkern -ldev -lsio -lblock -lfs -lusb -llwip -lptpd -leth -li2c -lrtc -lcan -lnand -lspi -lnor -lpwm -ladc -ltrace -lcounter -lc -lm")
 
-set(CMAKE_CXX_LINK_EXECUTABLE  "<CMAKE_CXX_COMPILER> <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> -nostartfiles -L${RTK}/lib/${ARCH}/${CPU} -T${RTK}/bsp/${BSP}/${BSP}.ld -Wl,--start-group <LINK_LIBRARIES> -l${BSP} -l${ARCH} -lkern -ldev -lsio -lblock -lfs -lusb -llwip -lptpd -leth -li2c -lrtc -lcan -lnand -lspi -lnor -lpwm -ladc -ltrace -lcounter -lstdc++ -lc -lm -Wl,--end-group")
-set(CMAKE_C_LINK_EXECUTABLE    "<CMAKE_C_COMPILER>   <FLAGS> <CMAKE_C_LINK_FLAGS>   <LINK_FLAGS> <OBJECTS> -o <TARGET> -nostartfiles -L${RTK}/lib/${ARCH}/${CPU} -T${RTK}/bsp/${BSP}/${BSP}.ld -Wl,--start-group <LINK_LIBRARIES> -l${BSP} -l${ARCH} -lkern -ldev -lsio -lblock -lfs -lusb -llwip -lptpd -leth -li2c -lrtc -lcan -lnand -lspi -lnor -lpwm -ladc -ltrace -lcounter -lc -lm -Wl,--end-group")
+set(CMAKE_CXX_LINK_EXECUTABLE "<CMAKE_CXX_COMPILER> <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> -nostartfiles -T${RTK}/bsp/${BSP}/${BSP}.ld -Wl,--start-group <LINK_LIBRARIES> ${RTK_LIBS} -Wl,--end-group")
+set(CMAKE_C_LINK_EXECUTABLE   "<CMAKE_C_COMPILER>   <FLAGS> <CMAKE_C_LINK_FLAGS>   <LINK_FLAGS> <OBJECTS> -o <TARGET> -nostartfiles -T${RTK}/bsp/${BSP}/${BSP}.ld -Wl,--start-group <LINK_LIBRARIES> ${RTK_LIBS} -Wl,--end-group")
