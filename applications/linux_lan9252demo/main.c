eda5a1dc76694ef169a1804ca540c8fe5ab740cc
DonBit <xoduddk123@naver.com>
2017-10-15 22:50:50 +0900

    Add lan9252 for Linux (#28)
    - Add kernel device driver
         drivers/linux/lan9252
     - Add lan9252 file for linux
         applications/linux_lan9252demo
         soes/hal/linux-lan9252
     - Fix linux compatibility for cc.h
         #ifdef __linux__

---
+++ applications/linux_lan9252demo/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * This function reads physical input values and assigns the corresponding members
+ * of Rb.Buttons
+ */
+void cb_get_Buttons()
+{
+
+}
+
+/**
+ * This function writes physical output values from the corresponding members of
+ * Wb.LEDs
+ */
+
+
+void cb_set_LEDs()
+{
+
+}
+
+
+/**
+ * This function is called after a SDO write of the object Cb.Parameters.
+ */
+void cb_post_write_Parameters(int subindex)
+{
+
+}
+
+void main_run(void * arg)
+{
+  soes_init();
+
+  while(1) {
+    soes();
+  }
+}
+
+int main(void)
+{
+   printf("Hello Main\n");
+   main_run(NULL);
+   return 0;
+}
+
