c0d3e17bcfb429bfc3651a2418f1ccdaf3f8678e
Lars Danielsson <lars.danielsson@rt-labs.com>
2020-09-14 11:42:38 +0200

    Export function SDO_findobject()
    Change-Id: I30b1c3b6a47111c1305aaa310bfdd882951689a1

--- soes/esc_coe.h
+++ soes/esc_coe.h
@@ -105,6 +105,7 @@ typedef struct
 
 void ESC_coeprocess (void);
 int16_t SDO_findsubindex (int16_t nidx, uint8_t subindex);
+int32_t SDO_findobject (uint16_t index);
 uint16_t sizeOfPDO (uint16_t index, int * nmappings, _SMmap * sm, int max_mappings);
 void SDO_abort (uint16_t index, uint8_t subindex, uint32_t abortcode);
 void COE_initDefaultValues (void);
