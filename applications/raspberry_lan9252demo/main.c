c2620736930720a90684547abd9b1e8f6394cfce
iwoodsawyer <ihoutzager@live.com>
2021-05-05 22:15:29 -0700

    Add HW watchdog check in application hook Enable HW watchdog and SM2 events in state change hook Improved comments and print statements

--- applications/raspberry_lan9252demo/main.c
+++ applications/raspberry_lan9252demo/main.c
@@ -13,8 +13,35 @@ uint8_t * txpdo = Obj.txpdo;
 uint8_t * rxpdo = Obj.rxpdo;
 #endif
 
+
+void cb_state_change (uint8_t * as, uint8_t * an)
+{
+   if (*as == SAFEOP_TO_OP)
+   {
+      /* Enable watchdog interrupt */
+      ESC_ALeventmaskwrite (ESC_ALeventmaskread() | ESCREG_ALEVENT_WD);
+      /* Enable SM2 interrupt */
+      ESC_ALeventmaskwrite (ESC_ALeventmaskread() | ESCREG_ALEVENT_SM2);
+   }
+}
+
+void cb_application (void)
+{
+   /* Check for watchdog expired event */
+   if (ESCvar.ALevent & ESCREG_ALEVENT_WD)
+   {
+      if ((ESC_WDstatus() & 0x01) &&
+          ((CC_ATOMIC_GET(ESCvar.App.state) & APPSTATE_OUTPUT) > 0))
+      {
+         DPRINT("DIG_process watchdog expired\n");
+         ESC_ALstatusgotoerror((ESCsafeop | ESCerror), ALERR_WATCHDOG);
+      }
+   }
+}
+
 void cb_get_inputs (void)
 {
+   /* Loop back outputs to inputs */
    #if DYN_PDO_MAPPING
    memcpy(Obj.txpdo,Obj.rxpdo,BYTE_NUM);
    #else
@@ -26,18 +53,28 @@ void cb_set_outputs (void)
 {
 }
 
-int main_run (void * arg)
+void cb_safeoutput (void)
+{
+   /* Set all outputs to zero */
+   #if DYN_PDO_MAPPING
+   memset(Obj.rxpdo,0,BYTE_NUM);
+   #else
+   memset(rxpdo,0,BYTE_NUM);
+   #endif
+}
+
+int soes (void * arg)
 {
    static esc_cfg_t config =
    {
-      .user_arg = "rpi3,cs0",
+      .user_arg = "rpi3,cs0", /* Change rpi3 to rpi4 if raspberry pi 4 or later */
       .use_interrupt = 0,
-      .watchdog_cnt = 0, /* Use HW SM watchdog instead */
+      .watchdog_cnt = UINT32_T, /* Use HW SM watchdog instead */
       .set_defaults_hook = NULL,
       .pre_state_change_hook = NULL,
-      .post_state_change_hook = NULL,
-      .application_hook = NULL,
-      .safeoutput_override = NULL,
+      .post_state_change_hook = cb_state_change,
+      .application_hook = cb_application,
+      .safeoutput_override = cb_safeoutput,
       .pre_object_download_hook = NULL,
       .post_object_download_hook = NULL,
       .rxpdo_override = NULL,
@@ -48,9 +85,10 @@ int main_run (void * arg)
       .esc_check_dc_handler = NULL,
    };
 
-   printf ("Start slave init\n");
+   DPRINT ("Start slave init\n");
    ecat_slv_init (&config);
-   printf ("Init finished\n");
+   DPRINT ("Initialization finished\n");
+   
    while (1)
    {
       ecat_slv();
@@ -61,7 +99,7 @@ int main_run (void * arg)
 
 int main (void)
 {
-   printf ("Hello Main\n");
-   main_run (NULL);
+   DPRINT ("SOES (Simple Open EtherCAT Slave)\n");
+   soes (NULL);
    return 0;
 }
